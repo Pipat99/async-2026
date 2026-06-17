18b67e786cfbb9fcf99bca6ac1fab08f16163511
Pipat99 <6710301003@cdti.ac.th>
2026-06-17 10:08:30 +0700

    add if else statement to calculate_ticket_price function to determine ticket price based on age.

--- WX/ticket.py
+++ WX/ticket.py
@@ -2,7 +2,14 @@ import sys
 
 def calculate_ticket_price(age):
     # --- เขียนโค้ดของนักเรียนในส่วนนี้ / Write your code here ---
+    if age < 12:
+        return 120
+    elif 12 <= age <= 60:
+        return 200
+    else:
+        return 150
     pass
+
     # --------------------------------------------------------
 
 def main():
